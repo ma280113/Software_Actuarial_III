6f86a0d92f6924d36dcf7f9fb335e9fcd5d8c06a
Manuel Juarez <juarezvivancoj@gmail.com>
2018-02-26 11:49:56 -0600

    Clase L260218
    Se agrego información de listas, matrices, cuadros de datos así como de los nombres de sus dimensiones y manejo de columnas y filas en vectores

--- Práctica de clase m210218.R
+++ Práctica de clase m210218.R
@@ -24,3 +24,36 @@
   as.numeric(w)
   as.character(w)
   as.numeric(c) #No es posible realizar la coerción
+#Para crear una lista de objetos, se usa la función list
+  #En una lista se conservan las clases de sus elementos
+    x<-list(1,"a",1+4i,T,1:5)
+#Para crear una matriz, se usa la función matrix la cual tiene como característica que todos sus elementos deben ser de una misma clase.
+  m<-matrix(nrow = 2,ncol = 3)
+  m<-matrix(c(1,2,3,1+2i,T,8),2,3)
+#Una manera de crear una matriz es tomar un vector y modificar sus dimensiones 
+  r<-1:20
+  dim(r)<-c(4,5)
+#Otra forma de crear una matriz es uniendo diferentes vectores
+  x<-1:3
+  y<-10:12
+  #La función cbind une por columnas los vectores
+    cbind(x,y)
+  #La función rbind une por filas
+    rbind(x,y)
+#Factores
+  x<-factor(c("si","si","no","no"))
+  #Los niveles se ordenan alfabéticamente y para mostrar sus cantidades se usa la función table(x)
+  table(x)
+  #Para identificar los niveles de cada entrada, se usa la función unclass()
+  unclass(x)
+#Para identificar los valores faltantes dentro de un vector se usa is.na()
+  x<-c(1,2,NA, 10,3)
+  is.na(x)
+#Identificamos los valores no númericos que no son faltantes con 
+  is.nan(x)
+#Para almacenar datos ordendados dentro de un cuadro usamos 
+  x<-data.frame(foo=1:4,bar=c(T,T,F,F))
+#Es posible asignar nombres a los elementos de un vector mediante la función names
+  x<-1:3
+  names(x)<-c("foo","bar","norf")
+  
